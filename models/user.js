0afc340e22cbe0aca4651053e07beef46ee46e0b
kmcclendon <kmcclendon56@gmail.com>
2022-08-12 18:55:12 -0500

    Fixed user schema

--- models/user.js
+++ models/user.js
@@ -2,14 +2,11 @@ const mongoose = require('mongoose');
 
 // Create your User Model
 const userSchema = new mongoose.Schema({
-	name: String,
+	email: String,
 	googleId: {
 	  type: String,
 	  required: true
 	},
-	email: String,
-    //want avatar??
-    //avatar: String
   }, {
 	timestamps: true
   });
